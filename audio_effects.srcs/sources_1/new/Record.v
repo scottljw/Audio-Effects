face019c8b60512791ccdd955b2d8dac1b81813b
Anonymous <someone@example.com>
2018-04-06 12:15:53 +0800

    Record

---
+++ audio_effects.srcs/sources_1/new/Record.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// AY1718 Sem 1 EE2020 Project
+// Project Name: Audio Effects
+// Module Name: AUDIO_FX_TOP
+// Team No.: Wednesday Group 06
+// Student Names: Gao Qikai, Li Jiawei
+// Matric No.: A0177350E, A0177400M
+// Description: Record and pitch shifting Module, record a 5s audio
+// 
+// Work Distribution:
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module record (
+    input clk_20k,
+    input 
+);
+    parameter TT = 5; // total time the module is going to record
